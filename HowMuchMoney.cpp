7cb3c60c2ad69055f53bcd2c37da3b2db31d589b
Azjae <azjaewashington@gmail.com>
2023-04-06 18:43:06 -0700

    Update HowMuchMoney.cpp

--- HowMuchMoney.cpp
+++ HowMuchMoney.cpp
@@ -1,115 +1,250 @@
 #include <iostream>
 #include <vector>
 #include <unordered_map>
+#include <iomanip>
 
 
 
-struct Person
+class Person
 {
-	std::unordered_map<int, double> money;
-	Person(int amountOfTwenties,int amountOfTens, int amountOfFives, int amountOfOnes)
+	std::unordered_map<int, int> dollarBills;
+	std::unordered_map<double, int> coins;
+	int billsUsed;
+	int coinsUsed;
+public:
+	Person(int amountOfBills, int amountOfCoins)
+		:billsUsed(0),coinsUsed(0)
 	{
-		money[20] = amountOfTwenties;
-		money[5] = amountOfFives;
-		money[10] = amountOfTens;
-		money[1] = amountOfOnes;
+		//initalize dollars
+		dollarBills[20] = amountOfBills;
+		dollarBills[5] = amountOfBills;
+		dollarBills[10] = amountOfBills;
+		dollarBills[1] = amountOfBills;
+
+		//initialize coins
+		coins[0.25] = amountOfCoins;
+		coins[0.10] = amountOfCoins;
+		coins[0.05] = amountOfCoins;
+		coins[0.01] = amountOfCoins;
 	}
 
+
+	//return dollar values
 	int getTwenties()
 	{
-		return money.find(20)->second;
+		return dollarBills.find(20)->second;
 	}
 	int getTens()
 	{
-		return money.find(10)->second;
+		return dollarBills.find(10)->second;
 	}
 	int getFives()
 	{
-		return money.find(5)->second;
+		return dollarBills.find(5)->second;
 	}
 	int getOnes()
 	{
-		return money.find(1)->second;
+		return dollarBills.find(1)->second;
+	}
+	int getAmountOfBillsUsed()
+	{
+		return billsUsed;
+	}
+
+	//return coin values
+	int getQuarters()
+	{
+		return coins.find(.25)->second;
+	}
+	int getDimes()
+	{
+		return coins.find(.10)->second;
+	}
+	int getNickels()
+	{
+		return coins.find(.05)->second;
+	}
+	int getPennies()
+	{
+		return coins.find(.01)->second;
+	}
+	int getBillsUsed()
+	{
+		return billsUsed;
+	}
+	int getCoinsUsed()
+	{
+		return coinsUsed;
 	}
 
 	void subtractMoney(int currency)
 	{
+		//subtracts dollar amounts
 		if (currency == 20)
 		{
-			--money.find(20)->second;
+			--dollarBills.find(20)->second;
 		}
 		else if (currency == 10)
 		{
-			--money.find(10)->second;
+			--dollarBills.find(10)->second;
 		}
 		else if (currency == 5)
 		{
-			--money.find(5)->second;
+			--dollarBills.find(5)->second;
 		}
 		else if (currency == 1)
 		{
-			--money.find(1)->second;
+			--dollarBills.find(1)->second;
+		}
+	}
+
+	void subtractMoney(double currency) //function overloading for double parameters (coins)
+	{
+		//subtracts coin amounts
+		if (currency == .25)
+		{
+			--coins.find(.25)->second;
+		}
+		else if (currency == .10)
+		{
+			--coins.find(.10)->second;
+		}
+		else if (currency == .05)
+		{
+			--coins.find(.05)->second;
+		}
+		else if (currency == .01)
+		{
+			--coins.find(.01)->second;
+		}
+	}
+
+	void transactionOccured(int type) //increases
+	{
+		if (type == 0)
+		billsUsed++;
+		if (type == 1)
+		{
+			coinsUsed++;
 		}
 	}
 
 	void DisplayMoney()
 	{
-		std::cout << "Twenties remaining: " << money[20] << '\n';
-		std::cout << "Tens remaining: " << money[10] << '\n';
-		std::cout << "Fives remaining: " << money[5] << '\n';
-		std::cout << "Ones remaining: " << money[1] << "\n\n";
+		std::cout << std::fixed << std::setprecision(2);
+		std::cout << "Twenties remaining: " << dollarBills[20] << '\n';
+		std::cout << "Tens remaining: " << dollarBills[10] << '\n';
+		std::cout << "Fives remaining: " << dollarBills[5] << '\n';
+		std::cout << "Ones remaining: " << dollarBills[1] << "\n\n";
+
+		std::cout << "Quarters remaining: " << coins[.25f] << '\n';
+		std::cout << "Dimes remaining: " << coins[.10f] << '\n';
+		std::cout << "Nickels remaining: " << coins[.05f] << '\n';
+		std::cout << "Pennies remaining: " << coins[.01f] << "\n\n";
+
 		std::cout << "Total amount: " << totalAmount() << "!\n\n";
+		std::cout << billsUsed << " dollar transactions!\n";
+		std::cout << coinsUsed << " coin transactions!\n";
 	}
 
 	double totalAmount()
 	{
-		double netWorth = money[20] * 20;
-		netWorth += money[10] * 10;
-		netWorth += money[5] * 5;
-		netWorth += money[1] * 1;
+		double netWorth = dollarBills[20] * 20;
+		netWorth += dollarBills[10] * 10;
+		netWorth += dollarBills[5] * 5;
+		netWorth += dollarBills[1] * 1;
+
+		netWorth += coins[.25] * .25;
+		netWorth += coins[.10] * .10;
+		netWorth += coins[.05] * .05;
+		netWorth += coins[.01] * .01;
 		return netWorth;
 	}
 };
 
-void howMuch(Person& obj, double& targetVal)
+void tradeMoney(Person& obj, float& targetVal)
 {
-	while (targetVal > 0)
+	int pennycounter = 0;
+	//traverses until dollars are exhausted
+	while (targetVal >= 0.01)
 	{
 		//if targetVal is more than 20 and the person has twenties 
 		if (targetVal >= 20 && obj.getTwenties() > 0)
 		{
 			obj.subtractMoney(20);
+			obj.transactionOccured(0);
 			targetVal -= 20;
 		}
 		//if targetVal is more than 10 and the person has tens 
 		else if (targetVal >= 10 && obj.getTens() > 0)
 		{
 			obj.subtractMoney(10);
+			obj.transactionOccured(0);
 			targetVal -= 10;
 		}
 		//if targetVal is more than 5 and the person has fives 
 		else if (targetVal >= 5 && obj.getFives() > 0)
 		{
 			obj.subtractMoney(5);
+			obj.transactionOccured(0);
 			targetVal -= 5;
 		}
+		//if targetVal is more than 1 and the person has ones 
 		else if (targetVal >= 1 && obj.getOnes() > 0)
 		{
 			obj.subtractMoney(1);
+			obj.transactionOccured(0);
 			targetVal -= 1;
 		}
+
+		//traverses until coins are exhausted
+		//if targetVal is more than .25 and the person has quarters 
+		else if (targetVal >= .25 && obj.getQuarters() > 0)
+		{
+			obj.subtractMoney(.25);
+			obj.transactionOccured(1);
+			targetVal -= .25;
+		}
+		//if targetVal is more than .10 and the person has dimes 
+		else if (targetVal >= .10 && obj.getDimes() > 0)
+		{
+			obj.subtractMoney(.10);
+			obj.transactionOccured(1);
+			targetVal -= .10;
+		}
+		//if targetVal is more than .05 and the person has nickels 
+		else if (targetVal >= .05 && obj.getNickels() > 0)
+		{
+			obj.subtractMoney(.05);
+			obj.transactionOccured(1);
+			targetVal -= .05;
+		}
+		//if targetVal is more than .01 and the person has pennies 
+		else if (targetVal >= .01 && obj.getPennies() > 0)
+		{
+			obj.subtractMoney(.01);
+			obj.transactionOccured(1);
+			targetVal -= .01;
+			pennycounter++;
+			if (pennycounter == 10)
+			{
+				break;
+			}
+		}
 	}
+
 }
 
+
 int main()
-{0
+{
 	std::cout << "Bob has the current amount of money on him...\n\n";
-	Person Bob(10,10,10,10);
+	Person Bob(10,10);
 	Bob.DisplayMoney();
 
 	std::cout << "How much would you like to borrow? Type an amount! ";
-	double target;
+	float target;
 	std::cin >> target;
-	howMuch(Bob, target);
+	tradeMoney(Bob, target);
 	Bob.DisplayMoney();
 }
